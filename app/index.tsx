62d4234fbfa205340ac3ab24dba7be7dccf0ba75
SP-1223 <yijunwai23@gmail.com>
2026-03-17 15:18:21 +1300

    Installed the react native camera library and added andriod permissions.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,6 @@
 import { View } from "react-native";
 import CameraButton from "../components/CameraButton";
 
-function onPressLearnMore() {
-  console.log("Learn more button pressed");
-}
-
 export default function Index() {
   return (
     <View
